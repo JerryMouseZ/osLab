e48bcad92dd2022aa3473b460272a56e43230ee7
jiangzhang <jiangzhangsg@qq.com>
2019-11-22 22:29:41 +0800

    lab3 complete

--- kern/trap.h
+++ kern/trap.h
@@ -3,7 +3,7 @@
 #ifndef JOS_KERN_TRAP_H
 #define JOS_KERN_TRAP_H
 #ifndef JOS_KERNEL
-# error "This is a JOS kernel header; user programs should not #include it"
+#error "This is a JOS kernel header; user programs should not #include it"
 #endif
 
 #include <inc/trap.h>
@@ -19,5 +19,21 @@ void print_regs(struct PushRegs *regs);
 void print_trapframe(struct Trapframe *tf);
 void page_fault_handler(struct Trapframe *);
 void backtrace(struct Trapframe *);
-
+void th0();
+void th1();
+void th2();
+void th3();
+void th4();
+void th5();
+void th6();
+void th7();
+void th8();
+void th9();
+void th10();
+void th11();
+void th12();
+void th13();
+void th14();
+void th16();
+void th_syscall();
 #endif /* JOS_KERN_TRAP_H */
